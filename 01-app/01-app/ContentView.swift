f5e1b26c2b49515995323a4d21d12d8d7cadadb3
Marília Luz <marilials2003@gmail.com>
2024-05-13 17:26:03 -0300

    cria arquivos iniciais do projeto

---
+++ 01-app/01-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-app
+//
+//  Created by Marília Luz dos Santos on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-app/01-app/_1_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_appApp.swift
+//  01-app
+//
+//  Created by Marília Luz dos Santos on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct _1_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
